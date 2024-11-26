c351516120a9c808f328cf19a4e78e6f5ea9a8cb
Supreme Intelligence <supremeintelligence1602@mail.ru>
2024-11-26 19:09:26 +0300

    Initial commit

---
+++ src/main/java/org/famcs/IndustrialProgrammingColoquium/Fibonacci.java
@@ -0,0 +1,7 @@
+package org.famcs.IndustrialProgrammingColoquium;
+
+public class Fibonacci 
+{
+
+
+}
---
+++ src/main/java/org/famcs/IndustrialProgrammingColoquium/IndustrialProgrammingColloquium.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package org.famcs.IndustrialProgrammingColoquium;
+
+/**
+ *
+ * @author supre
+ */
+public class IndustrialProgrammingColloquium 
+{
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
